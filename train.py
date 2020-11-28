83129cb028fd0cb7e5bb460bad1b6af42292ae3f
Tom Shlomo <tomshlomo@gmail.com>
2020-11-28 14:00:56 +0200

    fixed some pbars

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ import plotly.express as px
 import visualizations as vis
 from utils import accuracy
 
-model = UNetLight(n_channels=train_loader.dataset.num_channels, n_classes=train_loader.dataset.num_classes, k=32)
+model = UNetLight(n_channels=train_loader.dataset.num_channels, n_classes=train_loader.dataset.num_classes, k=16)
 optimizer = Adam(model.parameters(), lr=3e-4)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.2, patience=2, cooldown=2)
 loss_fcn = nn.CrossEntropyLoss(weight=train_loader.dataset.class_weights())
@@ -25,13 +25,18 @@ val_acc_list = []
 fig_train, fig_val = None, None
 lr_rate_list = []
 valid_loss_min = np.Inf  # track change in validation loss
-for epoch in range(1, n_epochs + 1):
+# epoch_bar = tqdm(range(1, n_epochs+1), position=0, desc=' Epochs', leave=True, postfix={'train_loss': 0.0,
+#                                                                             'train_acc': 0.0,
+#                                                                             'val_loss': 0.0,
+#                                                                             'val_acc': 0.0})
+epoch_bar = tqdm(range(1, n_epochs+1), desc=' Epochs')
+for epoch in epoch_bar:
     # train
     train_loss = 0.0
     train_acc = 0.0
     model.train()
-    bar = tqdm(train_loader, postfix={"train_loss": 0.0, "train_accuracy": 0.0})
-    for data, target in bar:
+    # bar = tqdm(train_loader, position=1, desc=' Training', postfix={"train_loss": 0.0, "train_acc": 0.0})
+    for data, target in train_loader:
         optimizer.zero_grad()
         output = model(data)
         batch_loss = loss_fcn(output, target)
@@ -40,7 +45,7 @@ for epoch in range(1, n_epochs + 1):
         train_loss += batch_loss.item() * data.shape[0]
         batch_acc = accuracy(output, target)
         train_acc += batch_acc * data.shape[0]
-        bar.set_postfix(ordered_dict={"train_loss": batch_loss.item(), "train_accuracy": batch_acc})
+        # bar.set_postfix(ordered_dict={"train_loss": batch_loss.item(), "train_acc": batch_acc})
     if epoch % plot_every == 1:
         fig_train = vis.plot(data[[0]], output[[0]], target[[0]], f'Epoch {epoch}, Train', fig_train)  # plot a single example
 
@@ -49,14 +54,14 @@ for epoch in range(1, n_epochs + 1):
     val_acc = 0.0
     model.eval()
     with torch.no_grad():
-        bar = tqdm(val_loader, postfix={"valid_loss": 0.0, "val_accuracy": 0.0})
-        for data, target in bar:
+        # bar = tqdm(val_loader, position=1, desc=' Validating', postfix={"val_loss": 0.0, "val_acc": 0.0})
+        for data, target in val_loader:
             output = model(data)
             batch_loss = loss_fcn(output, target)
             val_loss += batch_loss.item() * data.shape[0]
             batch_acc = accuracy(output, target)
             val_acc += batch_acc * data.shape[0]
-            bar.set_postfix(ordered_dict={"val_loss": batch_loss.item(), "val_accuracy": batch_acc})
+            # bar.set_postfix(ordered_dict={"val_loss": batch_loss.item(), "val_acc": batch_acc})
     if epoch % plot_every == 1:
         fig_val = vis.plot(data[[0]], output[[0]], target[[0]], f'Epoch {epoch}, Validation', fig_val)  # plot a single example
 
@@ -69,13 +74,17 @@ for epoch in range(1, n_epochs + 1):
     valid_loss_list.append(val_loss)
     train_acc_list.append(train_acc)
     val_acc_list.append(val_acc)
-    print('Epoch: {}  Training Loss: {:.6f}  Validation Loss: {:.6f}  '
-          'Training Accuracy: {:.6f}  Validation Accuracy: {:.6f}'
+    # epoch_bar.set_postfix({'train_loss': train_loss,
+    #                        'train_acc': train_acc,
+    #                        'val_loss': val_loss,
+    #                        'val_acc': val_acc})
+    epoch_bar.write('epoch: {}  train_loss: {:.3f}  val_loss: {:.3f}  '
+          'train acc: {:.3f}  val_acc: {:.3f}'
           .format(epoch, train_loss, val_loss, train_acc, val_acc))
 
     # save model (if improved on val set)
     if val_loss <= valid_loss_min:
-        print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(valid_loss_min, val_loss))
+        epoch_bar.write('Validation loss decreased ({:.3f} --> {:.3f}).  Saving model ...'.format(valid_loss_min, val_loss))
         torch.save(model.state_dict(), save_path)
         valid_loss_min = val_loss
 
